d0420eeba8d5d76d7f05ad14eb6d40023d572e8d
Geoffrey Teale <tealeg@gmail.com>
2012-04-09 23:18:51 +0200

    Now compiling with Go 1

--- workbook_test.go
+++ workbook_test.go
@@ -2,22 +2,21 @@ package xlsx
 
 import (
 	"bytes"
-	"os"
+	"encoding/xml"
 	"testing"
-	"xml"
 )
 
 // Test we can succesfully unmarshal the workbook.xml file from within
 // an XLSX file and return a XLSXWorkbook struct (and associated
 // children).
 func TestUnmarshallWorkbookXML(t *testing.T) {
-	var error os.Error
+	var error error
 	var buf = bytes.NewBufferString(`<?xml version="1.0" encoding="UTF-8" standalone="yes"?><workbook xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships"><fileVersion appName="xl" lastEdited="4" lowestEdited="4" rupBuild="4506"/><workbookPr defaultThemeVersion="124226"/><bookViews><workbookView xWindow="120" yWindow="75" windowWidth="15135" windowHeight="7620"/></bookViews><sheets><sheet name="Sheet1" sheetId="1" r:id="rId1"/><sheet name="Sheet2" sheetId="2" r:id="rId2"/><sheet name="Sheet3" sheetId="3" r:id="rId3"/></sheets><definedNames><definedName name="monitors" localSheetId="0">Sheet1!$A$1533</definedName></definedNames><calcPr calcId="125725"/></workbook>`)
 	var workbook *XLSXWorkbook
 	workbook = new(XLSXWorkbook)
-	error = xml.Unmarshal(buf, workbook)
+	error = xml.NewDecoder(buf).Decode(workbook)
 	if error != nil {
-		t.Error(error.String())
+		t.Error(error.Error())
 		return
 	}
 	if workbook.FileVersion.AppName != "xl" {
@@ -81,4 +80,3 @@ func TestUnmarshallWorkbookXML(t *testing.T) {
 		t.Error("workbook.CalcPr.CalcId != '125725'")
 	}
 }
-
