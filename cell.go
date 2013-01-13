bab8dd59022318cf5190355071e437c1825c71cb
zhcy <zhangchenyu2009@gmail.com>
2013-01-13 11:35:24 +0800

    add get border and color separate
    add get border and color separate

--- cell.go
+++ cell.go
@@ -20,24 +20,32 @@ type CellInterface interface {
 	String() string
 }
 
+// return cell's string value
 func (c *Cell) String() string {
 	return c.Value
 }
 
-func (c *Cell) GetStyle() *Style {
-	style := new(Style)
+// get cell borders(Left,Right,Top,Bottom)
+func (c *Cell) GetBorder() Border {
+	var border Border
 	if c.styleIndex > 0 && c.styleIndex < len(c.styles.CellXfs) {
 		xf := c.styles.CellXfs[c.styleIndex]
 		if xf.ApplyBorder != "0" {
-			var border Border
 			border.Left = c.styles.Borders[xf.BorderId].Left.Style
 			border.Right = c.styles.Borders[xf.BorderId].Right.Style
 			border.Top = c.styles.Borders[xf.BorderId].Top.Style
 			border.Bottom = c.styles.Borders[xf.BorderId].Bottom.Style
-			style.Boders = border
 		}
+	}
+	return border
+}
+
+// get cell fills(background color and foreground color)
+func (c *Cell) GetFill() Fill {
+	var fill Fill
+	if c.styleIndex > 0 && c.styleIndex < len(c.styles.CellXfs) {
+		xf := c.styles.CellXfs[c.styleIndex]
 		if xf.ApplyFill != "0" {
-			var fill Fill
 			if len(c.styles.Fills[xf.FillId].BgColor.Indexed) > 0 {
 				fill.BgColor = getColorFromIndexed(c.styles.Fills[xf.FillId].BgColor.Indexed)
 			} else {
@@ -48,16 +56,51 @@ func (c *Cell) GetStyle() *Style {
 			} else {
 				fill.FgColor = c.styles.Fills[xf.FillId].FgColor.Rgb
 			}
-			style.Fills = fill
 		}
 	}
+	return fill
+}
+
+// get cell styles (borders, fills..)
+func (c *Cell) GetStyle() *Style {
+	style := new(Style)
+	// get borders
+	style.Borders = c.GetBorder()
+	// get colors
+	style.Fills = c.GetFill()
+
+	// if c.styleIndex > 0 && c.styleIndex < len(c.styles.CellXfs) {
+	// 	xf := c.styles.CellXfs[c.styleIndex]
+	// 	if xf.ApplyBorder != "0" {
+	// 		var border Border
+	// 		border.Left = c.styles.Borders[xf.BorderId].Left.Style
+	// 		border.Right = c.styles.Borders[xf.BorderId].Right.Style
+	// 		border.Top = c.styles.Borders[xf.BorderId].Top.Style
+	// 		border.Bottom = c.styles.Borders[xf.BorderId].Bottom.Style
+	// 		style.Boders = border
+	// 	}
+	// 	if xf.ApplyFill != "0" {
+	// 		var fill Fill
+	// 		if len(c.styles.Fills[xf.FillId].BgColor.Indexed) > 0 {
+	// 			fill.BgColor = getColorFromIndexed(c.styles.Fills[xf.FillId].BgColor.Indexed)
+	// 		} else {
+	// 			fill.BgColor = c.styles.Fills[xf.FillId].BgColor.Rgb
+	// 		}
+	// 		if len(c.styles.Fills[xf.FillId].FgColor.Indexed) > 0 {
+	// 			fill.FgColor = getColorFromIndexed(c.styles.Fills[xf.FillId].FgColor.Indexed)
+	// 		} else {
+	// 			fill.FgColor = c.styles.Fills[xf.FillId].FgColor.Rgb
+	// 		}
+	// 		style.Fills = fill
+	// 	}
+	// }
 	return style
 }
 
 // Style is a high level structure intended to provide user access to
 // the contents of Style within an XLSX file.
 type Style struct {
-	Boders Border
+	Borders Border
 	Fills  Fill
 }
 
