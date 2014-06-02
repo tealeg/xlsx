fda077ff1f2e229b16e1e9672b97b5cdd34f4558
Geoffrey J. Teale <tealeg@gmail.com>
2014-06-02 23:29:44 +0200

    Test we can create a RefTable, populate it, use it to deference shared string IDs and derive an xlsxSST from it.

--- sharedstrings.go
+++ sharedstrings.go
@@ -4,8 +4,8 @@ package xlsx
 // http://schemas.openxmlformats.org/spreadsheetml/2006/main currently
 // I have not checked this for completeness - it does as much as need.
 type xlsxSST struct {
-	Count       string   `xml:"count,attr"`
-	UniqueCount string   `xml:"uniqueCount,attr"`
+	Count       int   `xml:"count,attr"`
+	UniqueCount int   `xml:"uniqueCount,attr"`
 	SI          []xlsxSI `xml:"si"`
 }
 
@@ -34,12 +34,20 @@ type xlsxR struct {
 // 	Data string `xml:"chardata"`
 // }
 
+
+type RefTable []string
+
+// NewSharedStringRefTable() creates a new, empty RefTable.
+func NewSharedStringRefTable() RefTable {
+	return RefTable{}
+}
+
 // MakeSharedStringRefTable() takes an xlsxSST struct and converts
 // it's contents to an slice of strings used to refer to string values
 // by numeric index - this is the model used within XLSX worksheet (a
 // numeric reference is stored to a shared cell value).
-func MakeSharedStringRefTable(source *xlsxSST) []string {
-	reftable := make([]string, len(source.SI))
+func MakeSharedStringRefTable(source *xlsxSST) RefTable {
+	reftable := make(RefTable, len(source.SI))
 	for i, si := range source.SI {
 		if len(si.R) > 0 {
 			for j := 0; j < len(si.R); j++ {
@@ -52,10 +60,24 @@ func MakeSharedStringRefTable(source *xlsxSST) []string {
 	return reftable
 }
 
-// ResolveSharedString() looks up a string value by numeric index from
+// NewXlsxSSTFromRefTable() takes a RefTable and returns and
+// equivalent xlsxSST representation.
+func NewXlsxSSTFromRefTable(refTable RefTable) xlsxSST {
+	sst := xlsxSST{}
+	sst.Count = len(refTable)
+	sst.UniqueCount = sst.Count
+	for _, ref := range refTable {
+		si := xlsxSI{}
+		si.T = ref
+		sst.SI = append(sst.SI, si)
+	}
+	return sst
+}
+
+// Resolvesharedstring() looks up a string value by numeric index from
 // a provided reference table (just a slice of strings in the correct
 // order).  This function only exists to provide clarity or purpose
 // via it's name.
-func ResolveSharedString(reftable []string, index int) string {
-	return reftable[index]
+func (rt RefTable) ResolveSharedString(index int) string {
+	return rt[index]
 }
