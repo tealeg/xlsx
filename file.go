f0f4c17408825cd695776cedca33df447efcded1
Geoffrey J. Teale <geoffrey.teale@canonical.com>
2014-09-10 00:05:10 +0200

    Add core.xml

--- file.go
+++ file.go
@@ -85,6 +85,10 @@ sheetIndex++
   <Application>Go XLSX</Application>
 </Properties>`
 
+	// TODO - do this properly, modification and revision information
+	parts["docProps/core.xml"] = `<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"></cp:coreProperties>`
+
 	xSST := refTable.makeXLSXSST()
 	parts["xl/sharedStrings.xml"], err = marshal(xSST)
 	sheetId := fmt.Sprintf("rId%d", sheetIndex)
