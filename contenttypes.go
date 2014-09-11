8a30f37eb51884a96196e8c748bc3470db13bfa8
Geoffrey Teale <geoffrey.teale@canonical.com>
2014-09-11 22:00:57 +0200

    Write parts to zip file.

--- contenttypes.go
+++ contenttypes.go
@@ -8,6 +8,7 @@ type xlsxTypes struct {
 	XMLName xml.Name `xml:"http://schemas.openxmlformats.org/package/2006/content-types Types"`
 
 	Overrides []xlsxOverride `xml:"Override"`
+	Defaults []xlsxDefault `xml:"Default"`
 }
 
 type xlsxOverride struct {
@@ -15,8 +16,15 @@ type xlsxOverride struct {
 	ContentType string `xml:",attr"`
 }
 
+type xlsxDefault struct {
+	Extension	string `xml:",attr"`
+	ContentType	string `xml:",attr"`
+}
+
 func MakeDefaultContentTypes() (types xlsxTypes) {
 	types.Overrides = make([]xlsxOverride, 7)
+	types.Defaults = make([]xlsxDefault, 2)
+
 	types.Overrides[0].PartName = "/_rels/.rels"
 	types.Overrides[0].ContentType = "application/vnd.openxmlformats-package.relationships+xml"
 	types.Overrides[1].PartName = "/docProps/app.xml"
@@ -31,5 +39,10 @@ func MakeDefaultContentTypes() (types xlsxTypes) {
 	types.Overrides[5].ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.styles+xml"
 	types.Overrides[6].PartName = "/xl/workbook.xml"
 	types.Overrides[6].ContentType = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet.main+xml"
+
+	types.Defaults[0].Extension = "rels"
+	types.Defaults[0].ContentType = "application/vnd.openxmlformats-package.relationships+xml"
+	types.Defaults[1].Extension = "xml"
+	types.Defaults[1].ContentType = "application/xml"
 	return
 }
