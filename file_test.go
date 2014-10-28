e147772d6034baef9ae84f8466b49b097b15d5c1
Geoffrey J. Teale <tealeg@gmail.com>
2014-10-28 16:52:12 +0100

    Move FileToSlice functionality and tests into file.go and file_test.go and kill the FileToSlice.go and FileToSlice_test.go files.

--- file_test.go
+++ file_test.go
@@ -3,6 +3,7 @@ package xlsx
 import (
 	"encoding/xml"
 	"path/filepath"
+
 	. "gopkg.in/check.v1"
 )
 
@@ -410,4 +411,34 @@ func (l *FileSuite) TestSaveFile(c *C) {
 	c.Assert(len(row1.Cells), Equals, 1)
 	cell1 = row1.Cells[0]
 	c.Assert(cell1.Value, Equals, "A cell!")
+}
+
+type SliceReaderSuite struct{}
+
+var _ = Suite(&SliceReaderSuite{})
+
+func (s *SliceReaderSuite) TestFileToSlice(c *C) {
+	output, err := FileToSlice("testfile.xlsx")
+	c.Assert(err, IsNil)
+	fileToSliceCheckOutput(c, output)
+}
+
+func (s *SliceReaderSuite) TestFileObjToSlice(c *C) {
+	f, err := OpenFile("testfile.xlsx")
+	output, err := f.ToSlice()
+	c.Assert(err, IsNil)
+	fileToSliceCheckOutput(c, output)
+}
+
+func fileToSliceCheckOutput(c *C, output [][][]string) {
+	c.Assert(len(output), Equals, 3)
+	c.Assert(len(output[0]), Equals, 2)
+	c.Assert(len(output[0][0]), Equals, 2)
+	c.Assert(output[0][0][0], Equals, "Foo")
+	c.Assert(output[0][0][1], Equals, "Bar")
+	c.Assert(len(output[0][1]), Equals, 2)
+	c.Assert(output[0][1][0], Equals, "Baz")
+	c.Assert(output[0][1][1], Equals, "Quuk")
+	c.Assert(len(output[1]), Equals, 0)
+	c.Assert(len(output[2]), Equals, 0)
 }
