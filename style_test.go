79e3d6c39516b49e8802666e1d4d34a70eab45a5
Geoffrey Teale <geoffrey.teale@canonical.com>
2014-11-16 12:14:05 +0100

    Add Fill data to TestMakeXLSXStyleElements.

--- style_test.go
+++ style_test.go
@@ -17,11 +17,15 @@ func (s *StyleSuite) TestMakeXLSXStyleElements(c *C) {
 	style := NewStyle()
 	font := *NewFont(12, "Verdana")
 	style.Font = font
-	xFont, _, _, _, _ := style.makeXLSXStyleElements()
+	fill := *NewFill("solid", "00FF0000", "FF000000")
+	style.Fill = fill
+	xFont, xFill, _, _, _ := style.makeXLSXStyleElements()
 	// HERE YOU ARE!
 	c.Assert(xFont.Sz.Val, Equals, "12")
 	c.Assert(xFont.Name.Val, Equals, "Verdana")
-
+	c.Assert(xFill.PatternFill.PatternType, Equals, "solid")
+	c.Assert(xFill.PatternFill.FgColor.RGB, Equals, "00FF0000")
+	c.Assert(xFill.PatternFill.BgColor.RGB, Equals, "FF000000")
 }
 
 type FontSuite struct{}
