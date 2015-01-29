fefc03707cee61e377d6f5da55a0e5ca17530d95
Eric <ericscottlagergren@gmail.com>
2015-01-29 13:50:04 -0800

    added int64(), setint64(), and (*Row)WriteSlice() functions

--- cell.go
+++ cell.go
@@ -90,6 +90,23 @@ func (c *Cell) Float() (float64, error) {
 	return f, nil
 }
 
+// Set a 64-bit integer
+func (c *Cell) SetInt64(n int64) {
+	c.Value = fmt.Sprintf("%d", n)
+	c.numFmt = "0"
+	c.formula = ""
+	c.cellType = CellTypeNumeric
+}
+
+// Returns the value of cell as 64-bit integer
+func (c *Cell) Int64() (int64, error) {
+	f, err := strconv.ParseInt(c.Value, 10, 64)
+	if err != nil {
+		return -1, err
+	}
+	return f, nil
+}
+
 // Set integer
 func (c *Cell) SetInt(n int) {
 	c.Value = fmt.Sprintf("%d", n)
@@ -99,6 +116,8 @@ func (c *Cell) SetInt(n int) {
 }
 
 // Returns the value of cell as integer
+// Has max 53 bits of precision
+// See: float64(int64(math.MaxInt))
 func (c *Cell) Int() (int, error) {
 	f, err := strconv.ParseFloat(c.Value, 64)
 	if err != nil {
