fa4768c1713c5912f4d349882ca7c7b93357e624
Colin Fox <colin.fox@cumul8.com>
2015-06-10 18:05:24 -0700

    Tweaked the row height custom flag

--- row.go
+++ row.go
@@ -1,14 +1,16 @@
 package xlsx
 
 type Row struct {
-	Cells  []*Cell
-	Hidden bool
-	Sheet  *Sheet
-	Height float64
+	Cells    []*Cell
+	Hidden   bool
+	Sheet    *Sheet
+	Height   float64
+	isCustom bool
 }
 
 func (r *Row) SetHeightCM(ht float64) {
 	r.Height = ht * 28.3464567 // Convert CM to postscript points
+	r.isCustom = true
 }
 
 func (r *Row) AddCell() *Cell {
