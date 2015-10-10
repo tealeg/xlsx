ade07d40042ed642048da874d46b27f8fcd309fb
Geoffrey J. Teale <geoffrey.teale@canonical.com>
2015-10-10 21:57:32 +0200

    Make AddSheet return error when trying to use a duplicate name.  Fixes #148.

--- file.go
+++ file.go
@@ -132,14 +132,17 @@ func (f *File) Write(writer io.Writer) (err error) {
 }
 
 // Add a new Sheet, with the provided name, to a File
-func (f *File) AddSheet(sheetName string) (sheet *Sheet) {
+func (f *File) AddSheet(sheetName string) (sheet *Sheet, err error) {
+	if _, exists := f.Sheet[sheetName]; exists {
+		return nil, fmt.Errorf("Duplicate sheet name '%s'.", sheetName)
+	}
 	sheet = &Sheet{Name: sheetName, File: f}
 	if len(f.Sheets) == 0 {
 		sheet.Selected = true
 	}
 	f.Sheet[sheetName] = sheet
 	f.Sheets = append(f.Sheets, sheet)
-	return sheet
+	return sheet, nil
 }
 
 func (f *File) makeWorkbook() xlsxWorkbook {
