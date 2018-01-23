adde01375fcca2672abee843161b5ec6c491dc0c
Vinicius Sanches <vinicius.sanches@gmail.com>
2018-01-23 14:17:11 -0200

    keep current AddSheet signature

--- stream_file_builder.go
+++ stream_file_builder.go
@@ -88,7 +88,7 @@ func NewStreamFileBuilderForPath(path string) (*StreamFileBuilder, error) {
 // AddSheet will add sheets with the given name with the provided headers. The headers cannot be edited later, and all
 // rows written to the sheet must contain the same number of cells as the header. Sheet names must be unique, or an
 // error will be thrown.
-func (sb *StreamFileBuilder) AddSheet(name string) error {
+func (sb *StreamFileBuilder) AddSheet(name string, headers []string, cellTypes []*CellType) error {
 	if sb.built {
 		return BuiltStreamFileBuilderError
 	}
@@ -100,6 +100,11 @@ func (sb *StreamFileBuilder) AddSheet(name string) error {
 		return err
 	}
 	sb.styleIds = append(sb.styleIds, []int{})
+
+	if headers != nil {
+		return sb.SetHeaders(headers, cellTypes)
+	}
+
 	return nil
 }
 
