319ba3bd5f3071ff9c5130ee821e181c745c38cf
DamianSzkuat <DamianSzkuat@hotmail.com>
2019-04-15 14:58:04 +0200

    Comments

--- stream_file_builder.go
+++ stream_file_builder.go
@@ -11,14 +11,22 @@
 // 5. Call NextSheet() to proceed to the next sheet. Once NextSheet() is called, the previous sheet can not be edited.
 // 6. Call Close() to finish.
 
+// Directions for using custom styles and different data types:
+// 1. Create a StreamFileBuilder with NewStreamFileBuilder() or NewStreamFileBuilderForPath().
+// 2. Use MakeStyle() to create the styles you want yo use in your document. Keep a list of these styles.
+// 3. Add all the styles you created by using AddStreamStyle() or AddStreamStyleList().
+// 4. Add the sheets and their column styles of data by calling AddSheetS().
+// 5. Call Build() to get a StreamFile. Once built, all functions on the builder will return an error.
+// 6. Write to the StreamFile with WriteS(). Writes begin on the first sheet. New rows are always written and flushed
+// to the io. All rows written to the same sheet must have the same number of cells as the number of column styles
+// provided when adding the sheet with AddSheetS() or an error will be returned.
+// 5. Call NextSheet() to proceed to the next sheet. Once NextSheet() is called, the previous sheet can not be edited.
+// 6. Call Close() to finish.
+
 // Future work suggestions:
-// Currently the only supported cell type is string, since the main reason this library was written was to prevent
-// strings from being interpreted as numbers. It would be nice to have support for numbers and money so that the exported
-// files could better take advantage of XLSX's features.
-// All text is written with the same text style. Support for additional text styles could be added to highlight certain
-// data in the file.
 // The current default style uses fonts that are not on Macs by default so opening the XLSX files in Numbers causes a
 // pop up that says there are missing fonts. The font could be changed to something that is usually found on Mac and PC.
+// Extend support for Formulas and Shared Strings.
 
 package xlsx
 
