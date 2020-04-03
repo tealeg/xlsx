f8720290b58572671b565bb11795c2305107e352
Arsen Abdusalamov <amabdusalamov@avito.ru>
2020-04-03 14:46:31 +0300

    check .Hidden flag for sheet in Marshal

--- file.go
+++ file.go
@@ -309,6 +309,10 @@ func (f *File) MarshallParts() (map[string]string, error) {
 		sheetPath := fmt.Sprintf("worksheets/sheet%d.xml", sheetIndex)
 		partName := "xl/" + sheetPath
 		relPartName := fmt.Sprintf("xl/worksheets/_rels/sheet%d.xml.rels", sheetIndex)
+		sheetState := sheetStateVisible
+		if sheet.Hidden {
+			sheetState = sheetStateHidden
+		}
 		types.Overrides = append(
 			types.Overrides,
 			xlsxOverride{
@@ -319,7 +323,7 @@ func (f *File) MarshallParts() (map[string]string, error) {
 			Name:    sheet.Name,
 			SheetId: sheetId,
 			Id:      rId,
-			State:   "visible"}
+			State:   sheetState}
 
 		worksheetMarshal, err := marshal(xSheet)
 		if err != nil {
