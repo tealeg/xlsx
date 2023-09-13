c481775c30743f659ff71dc5cda2a638997db40b
Geoffrey J. Teale <geoffrey@upvest.co>
2023-09-13 07:22:51 +0200

    Go fmt

--- reftable_test.go
+++ reftable_test.go
@@ -12,8 +12,7 @@ type RefTableSuite struct {
 	SharedStringsXML *bytes.Buffer
 }
 
-var reftabletest_sharedStringsXMLStr = (
-	`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+var reftabletest_sharedStringsXMLStr = (`<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 	<sst xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main"
 		 count="4"
 		 uniqueCount="4">
